bb719013ac94dbecc33727f4fa0af12d5c9ac4bf
serpent7776 <serpent7776@gmail.com>
2016-03-15 23:25:18 +0100

    added stage

--- core/src/com/github/serpent7776/GdxSample.java
+++ core/src/com/github/serpent7776/GdxSample.java
@@ -5,23 +5,38 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GdxSample extends ApplicationAdapter {
+
+	Stage stage;
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		final float delta = Gdx.graphics.getDeltaTime();
+		stage.act(delta);
+		stage.draw();
 	}
+
+	@Override
+	public void dispose () {
+		stage.dispose();
+	}
+
 }
