c00eb472245da2dc9b9c6e634ba5a29fb276e83e
serpent7776 <serpent7776@gmail.com>
2016-03-15 22:37:35 +0100

    initial commit

---
+++ desktop/src/com/github/serpent7776/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.serpent7776.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.serpent7776.GdxSample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxSample(), config);
+	}
+}
---
+++ ios/src/com/github/serpent7776/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.github.serpent7776;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.github.serpent7776.GdxSample;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new GdxSample(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
