8c71fba088236f29bbb83dd4ff266849145454c1
serpent7776 <serpent7776@gmail.com>
2016-03-17 23:14:00 +0100

    removed unused batch

--- core/src/com/github/serpent7776/GdxSample.java
+++ core/src/com/github/serpent7776/GdxSample.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -12,7 +11,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 public class GdxSample extends ApplicationAdapter {
 
 	Stage stage;
-	SpriteBatch batch;
 	Texture img;
 	Image image1;
 	Image image2;
@@ -21,7 +19,6 @@ public class GdxSample extends ApplicationAdapter {
 	public void create() {
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
-		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		image1 = new Image(img);
 		image2 = new Image(img);
@@ -33,9 +30,6 @@ public class GdxSample extends ApplicationAdapter {
 	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 		final float delta = Gdx.graphics.getDeltaTime();
 		stage.act(delta);
 		stage.draw();
